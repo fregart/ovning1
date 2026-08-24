58b37476a50e1dda54bf1ca0ba4ac6c36bdb0bb2
Julia <julia_ahlen@hotmail.com>
2026-08-24 21:12:43 +0200

    En till rad från Julia

--- Julia/Julia/Program.cs
+++ Julia/Julia/Program.cs
@@ -1,2 +1,3 @@
 ﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("This is a C# program using language version 14.0 and targeting .NET 10.0."); 
+Console.WriteLine("This is a C# program using language version 14.0 and targeting .NET 10.0.");
+Console.ReadLine();
